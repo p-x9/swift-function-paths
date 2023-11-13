db5ebac104983f7375462b197b188ad17d28bb5b
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-11-13 22:25:11 +0900

    format

--- Package.swift
+++ Package.swift
@@ -1,31 +1,27 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 import CompilerPluginSupport
 
 let package = Package(
     name: "FunctionPaths",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .macCatalyst(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FunctionPaths",
             targets: ["FunctionPaths"]
-        ),
-        .executable(
-            name: "FunctionPathsClient",
-            targets: ["FunctionPathsClient"]
-        ),
+        )
     ],
     dependencies: [
-        // Depend on the Swift 5.9 release of SwiftSyntax
         .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        // Macro implementation that performs the source transformation of a macro.
         .macro(
             name: "FunctionPathsMacros",
             dependencies: [
@@ -33,14 +29,12 @@ let package = Package(
                 .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
             ]
         ),
-
-        // Library that exposes a macro as part of its API, which is used in client programs.
-        .target(name: "FunctionPaths", dependencies: ["FunctionPathsMacros"]),
-
-        // A client of the library, which is able to use the macro in its own code.
-        .executableTarget(name: "FunctionPathsClient", dependencies: ["FunctionPaths"]),
-
-        // A test target used to develop the macro implementation.
+        .target(
+            name: "FunctionPaths",
+            dependencies: [
+                "FunctionPathsMacros"
+            ]
+        ),
         .testTarget(
             name: "FunctionPathsTests",
             dependencies: [
