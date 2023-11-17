b9706b93cde02ac2d447e8e601bec6d9b71b45fa
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-11-17 13:43:37 +0900

    add type alias

--- Sources/FunctionPaths/typealias.swift
+++ Sources/FunctionPaths/typealias.swift
@@ -9,5 +9,13 @@
 import Foundation
 
 public typealias FuncPath = FunctionPath
-
 public typealias FuncPathWithInput = FunctionPathWithInput
+
+public typealias ThrowingFuncPath = ThrowingFunctionPath
+public typealias ThrowingFuncPathWithInput = ThrowingFunctionPathWithInput
+
+public typealias AsyncFuncPath = AsyncFunctionPath
+public typealias AsyncFuncPathWithInput = AsyncFunctionPathWithInput
+
+public typealias AsyncThrowingFuncPath = AsyncThrowingFunctionPath
+public typealias AsyncThrowingFuncPathWithInput = AsyncThrowingFunctionPathWithInput
